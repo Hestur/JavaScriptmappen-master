aaa96899839eb1ffbaf15604931a98211bfdf816
hesten <spamhesten72@live.dk>
2019-02-27 12:32:00 +0100

    yesyes

---
+++ opgaver/OpgaverUge1js/test/main.js
@@ -0,0 +1,14 @@
+
+function onhovertxt() {
+    document.getElementById("teksten").classList.add("hilite");
+} // jeg ville have addet classen hilite til "teksten"
+
+function replaceimg() {
+    document.getElementById("billede").src = "../Galleri/img/Thumb/cat4.jpg";
+}
+// function showname() {
+//     document.getElementById("inpNavn").value = document.getElementById("navn").innerHTML;
+// }
+
+
+
