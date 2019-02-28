da9f65290ba29bc900931b3fb4e41056cf5a5055
hesten <spamhesten72@live.dk>
2019-02-28 09:08:31 +0100

    gg

--- APIs/XML-text/swapi.js
+++ APIs/XML-text/swapi.js
@@ -1,13 +1,13 @@
 callAPI();
 
 function callAPI(){
-    var r = "https://swapi.co/api/people/1/";
+    var r = "https://swapi.co/api/planets/3/";
     fetch(r,{
         method: "get"
      }).then(function(response){
          return response.json();
      }).then(function(jsonresponse){
-     //   console.log(jsonresponse.birth_year);
+      //  console.log(jsonresponse.name);
         printDataNy(jsonresponse);  
      }).catch(function(error){
          console.log("err" + error);
@@ -17,12 +17,12 @@ function callAPI(){
 var t ="";
 
 function printData(jsondata){
-t = "Name: " + jsondata.name + " eyecolor: " + jsondata.eye_color;
+t = "Name: " + jsondata.name;
 document.getElementById("result").innerHTML = t;
 }
 
 function printDataNy(jsondata){
-info = `P.name: ${jsondata.name} med hårfarve: ${jsondata.hair_color}`;
+info = `P.name: ${jsondata.name} Terrain: ${jsondata.terrain}`;
 document.getElementById("result").innerHTML = info;
 }
 
